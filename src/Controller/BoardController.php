22fbe17603e50838650beb270c2ae55da90a2267
Lakym972 <132657810+Lakym972@users.noreply.github.com>
2023-07-18 17:30:54 +0200

    MAJ Board

--- src/Controller/BoardController.php
+++ src/Controller/BoardController.php
@@ -17,4 +17,40 @@ class BoardController extends AbstractController
             'controller_name' => 'BoardController',
         ]);
     }
+
+    #[Route('/home', name: 'app_board_home')]
+    public function home(): Response
+    {
+        return $this->render('board/home.html.twig', [
+        ]);
+    }
+
+    #[Route('/cinema', name: 'app_board_cinema')]
+    public function cinema(): Response
+    {
+        return $this->render('board/cinema.html.twig', [
+        ]);
+    }
+
+    #[Route('/film', name: 'app_board_film')]
+    public function film(): Response
+    {
+        return $this->render('board/film.html.twig', [
+        ]);
+    }
+
+    #[Route('/reservation', name: 'app_board_reservation')]
+    public function reservation(): Response
+    {
+        return $this->render('board/reservation.html.twig', [
+        ]);
+    }
+
+    #[Route('/fidelite', name: 'app_board_fidelite')]
+    public function fidelite(): Response
+    {
+        return $this->render('board/fidelite.html.twig', [
+        ]);
+    }
+
 }
