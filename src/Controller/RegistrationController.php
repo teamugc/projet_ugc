86f04b4e918f7a4251a1dbe88167e426e0718166
bast4u <132562576+bast4u@users.noreply.github.com>
2023-07-19 19:22:56 +0200

    modif role admin

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -32,13 +32,13 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
                 );
-            // $user->setRoles(['ROLE_ADMIN']);
+            $user->setRoles(['ROLE_ADMIN']);
 
             $documentManager->persist($user);
             $documentManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_index');
         }
 
         return $this->render('registration/register.html.twig', [
